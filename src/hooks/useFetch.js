c0b9e5c4cdecf8d1b3fd2d44ab814b0db1b28011
Yanni0933 <yannnni0923@gmail.com>
2023-07-16 03:40:04 +0800

    React Fetch Data from Node Express API

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  // 一旦 url 發生改變，就會 call fetchData() 來獲取資料
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]);
+
+  // 就算 url 沒有發生改變，也可以靠 call reFetch() 來獲取資料
+  const reFetch = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(url);
+      setData(res.data);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+
+  return { data, loading, error, reFetch };
+};
+
+export default useFetch;
